2604ebb4a54162770bc8ebd35c6d7b0710547949
Brandon Ling <79050338+bzzling@users.noreply.github.com>
2024-10-16 18:43:28 -0400

    removed unecessary imports

--- lstm_model.py
+++ lstm_model.py
@@ -1,14 +1,5 @@
 import torch
 import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-import json
-from pprint import pprint
 from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
 
 output_dim = 1  # binary classification for thumbs up or down
--- model.py
+++ model.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 import json
 from pprint import pprint
 from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
